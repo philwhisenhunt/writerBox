b1c58b02c249fa015cac79505d226935c7224bd5
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-29 13:15:56 -0400

    pulling stuff a different way

--- resources/views/snippets/edit.blade.php
+++ resources/views/snippets/edit.blade.php
@@ -5,18 +5,17 @@
 
 
 <form action="">
+<div class="control">
+<input type="text" class="input" name="title" placeholder="Title" value="{{ $snippet->title }}">
+</div>
+<div class="class">
 
-<h2><textarea name="title-area" id="" cols="30" rows="2">
-{{$snippet->title}}
+<div class="control">
+    <textarea name="body-of-snippet" class="textarea" cols="100" rows="10">{{ $snippet->full_entry}}</textarea>
+</div>
 
-</textarea></h2>
 
-<p class="main-entry">
-<textarea name="" id="" cols="100" rows="10">
-{{ $snippet->full_entry }}
-</textarea>
-</p>
-
+</div>
 <button>Save</button>
 <button>Cancel</button>
 
