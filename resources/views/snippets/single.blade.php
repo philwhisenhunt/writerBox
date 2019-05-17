540e3545df72a3b1c35c1a56983d61d57dc55f22
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-05-16 22:31:02 -0400

    Adding buttons to single snippet view

--- resources/views/snippets/single.blade.php
+++ resources/views/snippets/single.blade.php
@@ -5,5 +5,9 @@
      <h3>{{ $snippet->title }}</h3>
      <p>{{ $snippet->full_entry }}</p>
      @endforeach
+    <div class="main-content">
 
+     <button class="editing-buttons">Edit</button>
+     <button class="editing-buttons">Delete</button>
+     </div>
 @endsection
