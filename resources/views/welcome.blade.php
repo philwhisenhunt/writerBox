e08794de12e6dcc0fbb088d9054455343f4299ad
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-06-02 19:38:31 -0400

    Trying to move the buttons to match rest of site, so far having trouble

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,13 @@
 
 @extends('layout')
 
+
 @section('content')
 
 
-<div class="main-content">
-<!-- <a href="/snippets">View All Snippets</a> -->
-<a href="/snippets"><button> View All Snippets</button></a>
+   <a href="/snippets"><button class="editing-buttons"> View All Snippets</button></a>
 
-
-<form action="">
-    <div class="control">
-    </div>
  
 
-</form>
-</div>
 
 @endsection
