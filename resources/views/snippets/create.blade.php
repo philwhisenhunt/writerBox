887ac57ddef2853d98b932d60f13e805aa25531d
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-06-28 23:42:49 -0400

    trying out a form method, first step is to add it

--- resources/views/snippets/create.blade.php
+++ resources/views/snippets/create.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="main-content">
-<form action="POST">
+<form method="POST">
     <div class="control">
         <input type="text" class="input" name="title" placeholder="Title" value="">
     </div>
